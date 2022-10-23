375680c344a566686480bbabebc7fce4122267ee
Yasmine77 <mejriyasmine77@gmail.com>
2022-10-23 20:15:10 +0100

    State CheckPoint

--- src/App.js
+++ src/App.js
@@ -2,23 +2,58 @@ import logo from './logo.svg';
 import profilepng from './profilepng.png'
 import './App.css';
 import  Profile  from './profile/profileComponent';
-function App() {
+import React from 'react';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    FullName:"",
+    bio:"",
+    imgSrc:"",
+    profession:"",
+    Show:false
+  };
+ 
+  this.handleShow=this.handleShow.bind(this)
+  }
+  handleShow=()=>{
+    const {Show}=this.state;
+    
+    this.setState({Show :!Show})
+}
+componentDidMount() {
+  const interval = setInterval(()=>{
+ 
+},40000);
+
+}
+
+
+  render(){
   const alertMyinput=FullName=>alert(FullName)
   console.log(profilepng)
-  
+  const styleBtn={backgroundColor: "#4CAF50",
+        border: "none",
+        color: "white",
+        padding: 15,
+        textAlign: "center",
+        textDecoration: "none",
+        fontSize: 30,}
   return (
     <div className="App">
       <header className="App-header">
-       <Profile 
+        <button  style={styleBtn} onClick={this.handleShow}> Show My Profile</button>
+      
+{this.state.Show && <Profile 
        img={profilepng}
        FullName="Yasmine Mejri"
        bio="I'm a junior full stack devloper Learning React"
        profession="Junior Full Stack"
-       alertMyinput={alertMyinput}/>
-
+       alertMyinput={alertMyinput}/>}
       </header>
+      
     </div>
   );
 }
-
+}
 export default App;
